24b8ec2caa1c954154566b61475ad6a2b12b263b
torsten <torsten@de.ibm.com>
2021-12-16 10:53:34 +0100

    Initial copy data volume tracking

--- cli_command_execution.py
+++ cli_command_execution.py
@@ -10,8 +10,6 @@ url_file = open('final_commands.txt','r')
 
 lines = url_file.readlines()
 from subprocess import PIPE, Popen
-#print(lines)
-
 
 @ray.remote(num_cpus=0.25)
 def do_it(filen, input_endpoint, output_endpoint, input_access_key, input_secret_key, output_access_key, output_secret_key):
@@ -25,7 +23,6 @@ def do_it(filen, input_endpoint, output_endpoint, input_access_key, input_secret
     transferred = output[output.find("Transferred: "):output.find("Speed: ")].split(" ")
     transferred_str = transferred[1] + transferred[2]
     return(transferred_str)
-    #print("Transferred: {}".format(humanfriendly.parse_size(transferred_str)))
 
 if __name__ == '__main__':
     ray.init(address='auto')
@@ -42,7 +39,9 @@ if __name__ == '__main__':
     result_ids = all_ref_objs
     start_time = time.time()
     while len(result_ids) > 0:
-        done_id, result_ids = ray.wait(result_ids)
+        done_ids, result_ids = ray.wait(result_ids)
+        for done_id in done_ids:
+            print("\033[96m(Main loop)\033[0m Transferred: {}".format(humanfriendly.parse_size(ray.get(done_id))))
         now_seconds = time.time()
         duration=now_seconds-start_seconds
         print("\033[96m(Main loop)\033[0m Tasks left: {}/{}".format(len(result_ids), len(all_ref_objs)))
